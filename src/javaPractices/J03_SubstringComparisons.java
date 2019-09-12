1a7609467c8afb254556e218bd67fd4652057f1b
kevin ngo <phucaone@gmail.com>
2019-09-12 08:01:44 -0700

    updated new problem (unsolved)

---
+++ src/javaPractices/J03_SubstringComparisons.java
@@ -0,0 +1,41 @@
+package javaPractices;
+
+/*	Given a string s and an integer k 
+ *  Finds the lexicographically smallest and largest substrings of length k.
+ *  
+ *  Input Format
+	The first line contains a string denoting s. 
+	The second line contains an integer denoting k.
+ *	
+ *	Constraints:
+ 	1 <= s <= 100
+ 	s consists of English alphabetic letters only 
+ *
+ *	Output
+ 	Return the respective lexicographically smallest and largest substrings 
+ 			as a single newline-separated string.
+ *	
+ *	Sample Input 0
+ 	*	Input: welcometojava 
+ 	
+ 	*	Output:
+		 		ava
+				wel
+				
+	*	Explanation:
+		String s has the following lexicographically-ordered substrings of length k:
+			["ava", "com", "elec", "eto", "jav", "lco", "met", "oja", "ome", "toj", "wel"]
+			
+		=> smallest substrings (first): ava
+		=> largest substrings (last): wel 
+ *
+*/
+
+public class J03_SubstringComparisons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
